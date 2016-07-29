542a70f15f103b822bba98cd144e5fddd5e6f302
Sawyer X <xsawyerx@cpan.org>
2016-07-30 01:40:42 +0200

    document some

--- lib/Dancer2/Plugin/ParamTypes.pm
+++ lib/Dancer2/Plugin/ParamTypes.pm
@@ -162,3 +162,58 @@ sub run_check {
 }
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    package MyApp {
+        use Dancer2;
+        use Dancer2::Plugin::ParamTypes;
+
+        get '/' => with_types[(
+            {
+                'name'       => 'id',
+                'source'     => 'query',
+                'type'       => 'positive_int',
+                'on_invalid' => 'error',
+            },
+        )} => sub {
+            my $id = query_parameters->get('id');
+
+            # $id is a positive int, for sure
+        };
+    }
+
+=head1 DESCRIPTION
+
+This is a basic module that allows you to provide a stanza of
+parameter type checks for your routes.
+
+It supports all three possible sources (C<route>, C<query>, and
+C<body>) and allows you to both add your own checks and your own
+actions when a check fails.
+
+=head1 TO DO
+
+=over 4
+
+=item * Add more types
+
+Trivial examples are available as C<int>, C<positive_int>, and
+C<negative_int>. A lot of other common types should be added.
+
+=item * Support type systems
+
+There are several type systems available. There's not a lot of use
+writing another one unless special ones are added. Some type systems
+already allow you to add advanced options like in-lined version of
+your types.
+
+L<Type::Tiny> comes to mind.
+
+=item * Document the rest of the options
+
+Duh.
+
+=back
